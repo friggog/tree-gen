565bc4d691bdbe0bf7f69aea46303667279381aa
Luke Jones <luketimothyjones@users.noreply.github.com>
2018-04-04 23:39:11 -0500

    Remove unused registrations

--- ch_trees/__init__.py
+++ ch_trees/__init__.py
@@ -10,15 +10,11 @@ from . import gui
 def register():
     bpy.utils.register_class(gui.TreeGen)
     bpy.utils.register_class(gui.TreeGenPanel)
-    bpy.utils.register_class(gui.TreeGenLogger)
-    # bpy.types.Object.expanded = bpy.props.BoolProperty(default=True)
 
 def unregister():
     # Reversing order is best-practice
-    bpy.utils.unregister_class(gui.TreeGenLogger)
     bpy.utils.unregister_class(gui.TreeGenPanel)
     bpy.utils.unregister_class(gui.TreeGen)
-    # del bpy.types.Object.expanded
 
 
 if __name__ == "__main__":
