a31ed4e8d2cbc712a0ffa579281130b52a90bfe2
Luke Jones <underdweller@gmail.com>
2018-04-08 17:20:57 -0500

    Better geometry simplification memory management
    Drastically decreases the memory footprint of simplify_branch_geometry
    via better code execution order, explicit data removes (rather than by
    relying garbage collection after unlinking the object), and removing
    object references.

--- ch_trees/utilities.py
+++ ch_trees/utilities.py
@@ -14,32 +14,38 @@ def simplify_branch_geometry(context, angle_limit=1.5):
     """
     
     scene = context.scene
-    tree = scene.objects.active
+    
+    try:
+        tree = scene.objects.active
+    except AttributeError:
+        raise Exception('Could not find tree while attempting to simplify branch geometry')
     
     try:
         old_branches = [child for child in tree.children if 'Branches' in child.name][0]
     except IndexError:
-        raise Exception('No branches found while simplifying tree geometry')
+        raise Exception('No branches found while simplifying branch geometry')
 
-    # Convert the branches curve to a mesh, then create an object to contain it
-    branches_mesh = old_branches.to_mesh(scene, False, 'RENDER')
-    new_branches = bpy.data.objects.new('branches_mesh', branches_mesh)
-
-    # Make the mesh active in the world, then associate it with the tree
+    
+    # Convert the branches curve to a mesh, then get an editable copy
+    br_bmesh = bmesh.new()
+    br_bmesh.from_mesh(old_branches.to_mesh(scene, False, 'RENDER'))
+    
+    # Remove the old branches from the scene and purge them from memory
+    scene.objects.unlink(old_branches)
+    bpy.data.curves.remove(old_branches.data)
+    bpy.data.objects.remove(old_branches)
+    
+    # Perform a limited dissolve
+    bmesh.ops.dissolve_limit(br_bmesh, verts=br_bmesh.verts, edges=br_bmesh.edges, angle_limit=radians(angle_limit))
+    
+    # Create a new mesh and container object
+    new_branches = bpy.data.objects.new('Branches', bpy.data.meshes.new('branches'))
+    br_bmesh.to_mesh(new_branches.data)
+    
+    # Purge bmesh from memory
+    br_bmesh.free()
+    
+    # Make the mesh active in the scene, then associate it with the tree
     scene.objects.link(new_branches)
     new_branches.matrix_world = tree.matrix_world
     new_branches.parent = tree
-
-    # Get an editable copy of the mesh, perform a limited dissolve, then replace the old mesh
-    br_bmesh = bmesh.new()
-    br_bmesh.from_mesh(new_branches.data)
-    bmesh.ops.dissolve_limit(br_bmesh, verts=br_bmesh.verts, edges=br_bmesh.edges, angle_limit=radians(angle_limit))
-    br_bmesh.to_mesh(new_branches.data)
-    
-    # Get bmesh out of memory ASAP
-    br_bmesh.clear()
-    br_bmesh.free()
-
-    # Remove the old branches from the world and finalize the name
-    scene.objects.unlink(old_branches)
-    new_branches.name = 'Branches'
