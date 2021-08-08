f8e4f9ffe643cec59443e91c97f852e26d872a4e
Charlie Hewitt <contact@chewitt.me>
2021-08-08 10:11:06 +0100

    Bump version

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-bl_info = {
+nbl_info = {
     "name": "TreeGen",
     "category": "Object",
     "description": "Generate high quality tree models",
     "author": "Charlie Hewitt and Luke Pflibsen-Jones",
-    "version": (0, 0, 2),
+    "version": (0, 0, 3),
     "wiki_url": "https://github.com/friggog/tree-gen/wiki",
     "tracker_url": "https://github.com/friggog/tree-gen/issues/new/choose",
     'blender': (2, 80, 0)
